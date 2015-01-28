48dfedbae526504dbcb6ed3109272117838e4488
Joseph Won <godspeedyoo@gmail.com>
2015-01-27 17:13:27 -0800

    moved route methods into route model

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -36,32 +36,7 @@ end
 
 #
 
-Route = Struct.new(:route) do
 
-  def list_inbound_stops
-    list = {}
-    self.inbound_stops.each do |stop|
-      list[stop["table"]["tag"]] = stop["table"]["title"]
-    end
-    list
-  end
-
-  def list_outbound_stops
-    list = {}
-    self.outbound_stops.each do |stop|
-      list[stop["table"]["tag"]] = stop["table"]["title"]
-    end
-    list
-  end
-
-  def inbound_stops
-    route["table"]["directions"][0]["table"]["stops"]
-  end
-
-  def outbound_stops
-    route["table"]["directions"][1]["table"]["stops"]
-  end
-end
 
 # r12 = Muni::Route.find(12)
 # p r12.outbound.stop_at("2nd St & Folsom St").predictions
