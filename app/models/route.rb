e1d37144f71aa3d8b67726f25a95dbd700783868
Joseph Won <godspeedyoo@gmail.com>
2015-01-27 17:32:00 -0800

    Moved route accessor struct class into route helper file

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,33 +1,8 @@
 class Route < ActiveRecord::Base
   # Remember to create a migration!
-  attr_reader :route
 
-  def initialize(route)
-    @route = route
-  end
-
-  def list_inbound_stops
-    list = {}
-    self.inbound_stops.each do |stop|
-      list[stop["table"]["tag"]] = stop["table"]["title"]
-    end
-    list
-  end
-
-  def list_outbound_stops
-    list = {}
-    self.outbound_stops.each do |stop|
-      list[stop["table"]["tag"]] = stop["table"]["title"]
-    end
-    list
-  end
-
-  def inbound_stops
-    route["table"]["directions"][0]["table"]["stops"]
-  end
-
-  def outbound_stops
-    route["table"]["directions"][1]["table"]["stops"]
-  end
 
 end
+
+
+
